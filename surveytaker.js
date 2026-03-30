8f8a51ff640f34609bab132a1a851e80d8653c5f
sostoeckel <so.stoeckel@gmail.com>
2026-03-30 19:29:26 -0400

    Add files via upload

--- surveytaker.js
+++ surveytaker.js
@@ -1,11 +1,9 @@
-
-
 function headerload(){
     const element = document.getElementById("surveytakerheader")
     var surveys = 0
 
     if(surveys == 0){
-        element.textContent = "There are no surveys to take. Try making a survey!";
+        element.textContent = "There are no surveys to take. Try making or importing a survey!";
     } else {
         element.textContent = "Take a Survey"
     }
