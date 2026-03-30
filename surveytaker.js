378691087ec627ca03616244c360b3ce3434073e
Alex Nemirovsky <146397626+alexn41@users.noreply.github.com>
2026-03-30 15:54:05 -0400

    Add files via upload

---
+++ surveytaker.js
@@ -0,0 +1,12 @@
+
+
+function headerload(){
+    const element = document.getElementById("surveytakerheader")
+    var surveys = 0
+
+    if(surveys == 0){
+        element.textContent = "There are no surveys to take. Try making a survey!";
+    } else {
+        element.textContent = "Take a Survey"
+    }
+}
