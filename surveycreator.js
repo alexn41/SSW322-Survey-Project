2cca8b6b104815c6f7e8b7e9a6345a31c0924608
mfriedma1-svg <mfriedma1@stevens.edu>
2026-04-26 17:00:27 -0400

    encryption part 2 yahoo

--- surveycreator.js
+++ surveycreator.js
@@ -190,21 +190,46 @@ function encrypt(text, key) {
   return btoa(result);
 }
 
+function decrypt(encodedText, key) {
+  let text = atob(encodedText);
+  let result = '';
+
+  for (let i = 0; i < text.length; i++) {
+    const textChar = text.charCodeAt(i);
+    const keyChar = key.charCodeAt(i % key.length);
+
+    // Reverse the shift
+    let decryptedChar = textChar - keyChar;
+    if (decryptedChar < 0) decryptedChar += 65535;
+
+    result += String.fromCharCode(decryptedChar);
+  }
+
+  return result;
+}
+
 function importForm() {
 
     var form = document.getElementById("form");
     var doc = document.getElementById("import");
-    var text = String(doc.value);
+    var evil_text = String(doc.value);
+    console.log(evil_text)
+    if(evil_text.length == 0) {
+        return;
+    }
+    var text = decrypt(evil_text, key)
+    form.replaceChildren();
     var data = text.split("|")
     var count = 1
     for(let s=0;s<data.length;s++) {
         var segment = data[s].split(",")
-        switch(segment[0].length) {
-            case 3: // text input question or any answer
-                if(segment[0].charAt(1) == "q") {
+        var true_len = segment[0].length - String(count).length
+        switch(true_len) {
+            case 2: // text input question or any answer
+                if(segment[0].charAt(1) == "q") { // text input question
                     var number = document.createElement("label");
                     number.for = "#q"+count;
-                    var text = document.createTextNode("Question "+count);
+                    var text = document.createTextNode("Question "+count+": "+segment[1]);
                     number.appendChild(text);
                     form.appendChild(number);
                     form.appendChild(document.createElement("br"));
@@ -213,52 +238,66 @@ function importForm() {
                     question.type = "text";
                     question.name = "#q"+count;
                     question.id = "#q"+count;
-                    question.value = segment[1];
+                    question.placeholder = "Enter Answer";
                     form.appendChild(question);
                     form.appendChild(document.createElement("br"))
                     break;
                 }
-                else {
-                    var answer = document.createElement("input");
-                    answer.type = "text";
-                    answer.name = "#a"+count;
-                    answer.id = "#a"+count;
-                    answer.value = segment[1];
-                    form.appendChild(answer);
-                    form.appendChild(document.createElement("br"));
+                else { // answer
+                    answers.push(segment[1])
                     form.appendChild(document.createElement("br"));
                     count++;
                     break;
                 }
-            case 4: // mult. question
+            case 3: // mult. question
                 var number = document.createElement("label");
                 number.for = "#q"+count;
-                var text = document.createTextNode("Question "+count);
+                var text = document.createTextNode("Question "+count+": "+segment[1]);
                 number.appendChild(text);
                 form.appendChild(number);
                 form.appendChild(document.createElement("br"));
 
-                var question = document.createElement("input");
-                question.type = "text";
-                question.name = "#q"+count+segment[0].charAt(3);
-                question.id = "#q"+count+segment[0].charAt(3);
-                question.value = segment[1];
-                form.appendChild(question);
-                form.appendChild(document.createElement("br"));
+                state = segment[0].charAt(3); // stores multiple choice or answer
                 break;
-            case 5: // mult. option
+            case 4: // mult. option
+                if(state == "c") { //multiple choice option
+
+                    var question = document.createElement("input");
+                    question.type = "radio";
+                    question.name = "#q"+count+"0";
+                    question.id = "#q"+count+segment[0].charAt(4);
+                    question.value = segment[1];
+                    form.appendChild(question);
+
+                    var number = document.createElement("label");
+                    number.for = "#q"+count+segment[0].charAt(4);
+                    var text = document.createTextNode(segment[1]);
+                    number.appendChild(text);
+                    form.appendChild(number);
+                    form.appendChild(document.createElement("br"));
+                    break;
+
+                } else { //multiple answer option
+
                 var question = document.createElement("input");
-                question.type = "text";
+                question.type = "checkbox";
                 question.name = "#q"+count+segment[0].charAt(4);
                 question.id = "#q"+count+segment[0].charAt(4);
                 question.value = segment[1];
                 form.appendChild(question);
+
+                var number = document.createElement("label");
+                number.for = "#q"+count+segment[0].charAt(4);
+                var text = document.createTextNode(segment[1]);
+                number.appendChild(text);
+                form.appendChild(number);
                 form.appendChild(document.createElement("br"));
+
                 break;
+
+                }
             default:
                 break;
         }
     }
-
-    
 }
